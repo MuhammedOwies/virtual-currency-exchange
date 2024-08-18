a28b96c517948eb7c05d8da8db9864be17496ada
muhammed.ramadan <muhammed.ramadan@sbs-sa.net>
2024-08-19 00:25:49 +0300

    .

---
+++ src/main/java/com/blockchain/virtualcurrencyexchange/VirtualCurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.blockchain.virtualcurrencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCurrencyExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualCurrencyExchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blockchain/virtualcurrencyexchange/VirtualCurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockchain.virtualcurrencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCurrencyExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
